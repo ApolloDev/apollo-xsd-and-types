f901d5dbba9866d990b8de776e9d4b53fe170a7b
Adam Darr <adamdarr@gmail.com>
2017-06-16 12:35:57 -0400

    update xrange

--- src/main/scripts/apollo-xsd-update/excel2csv.py
+++ src/main/scripts/apollo-xsd-update/excel2csv.py
@@ -1,14 +1,20 @@
 def excel2CSV(ExcelFile, SheetName, CSVFile):
-	import xlrd
-	import csv
-	workbook = xlrd.open_workbook(ExcelFile)
-	worksheet = workbook.sheet_by_name(SheetName)
-	csvfile = open(CSVFile, 'wb')
-	wr = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    import xlrd
+    import csv
 
-	for rownum in xrange(worksheet.nrows):
-		wr.writerow(
-			list(x.encode('utf-8') if type(x) == type(u'') else x
-				for x in worksheet.row_values(rownum)))
+    try:
+        xrange
+    except NameError:
+        xrange = range
+    
+    workbook = xlrd.open_workbook(ExcelFile)
+    worksheet = workbook.sheet_by_name(SheetName)
+    csvfile = open(CSVFile, 'wb')
+    wr = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
 
-	csvfile.close()
+    for rownum in xrange(worksheet.nrows):
+        wr.writerow(
+            list(x.encode('utf-8') if type(x) == type(u'') else x
+                for x in worksheet.row_values(rownum)))
+
+    csvfile.close()
